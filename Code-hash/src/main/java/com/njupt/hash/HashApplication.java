93c1b4cc04fb011723efe005a9f76a2c00ac2abe
wall <798657709@qq.com>
2019-06-19 21:19:15 +0800

    百度开源的UID/一致Hash算法

---
+++ Code-hash/src/main/java/com/njupt/hash/HashApplication.java
@@ -0,0 +1,13 @@
+package com.njupt.hash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HashApplication.class, args);
+    }
+
+}
